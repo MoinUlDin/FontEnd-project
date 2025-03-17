b421590c75966395681a5e992c169e96aa6f8068
moin <moinuldinc@gmail.com>
2025-03-17 12:35:03 +0500

    URL http://

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -5,7 +5,8 @@ class ApiClient {
   constructor() {
     this.client = axios.create({
       // Change URL
-      baseURL: "hrtestconductorbackend-04f6c2-1b196c-65-108-245-140.traefik.me",
+      baseURL:
+        "http://hrtestconductorbackend-04f6c2-1b196c-65-108-245-140.traefik.me",
       // baseURL: "http://localhost:8000",
       headers: { "Content-Type": "application/json" },
     });
